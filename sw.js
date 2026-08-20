7d1ac1f1ab250e17edba190d579d8be9ce976674
rodrigoeqs <133398159+rodrigoeqs@users.noreply.github.com>
2026-08-20 04:39:40 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="rosario-fisico-v4";const A=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))));
